df9cdb6d91a2c25f09cecd8687ff70ad49f30e1a
Bill Morgan <w@wamorgan.com>
2013-12-23 05:33:17 +0000

    checkpoint

--- sema.cpp
+++ sema.cpp
@@ -31,7 +31,7 @@ using namespace std;
 #define	PGM_NAME		"Sema"
 #define	PGM_VERSION		"1.0"
 #define	OPTION_STRING	"?Bc:n:uv"
-#define	PATH_MAX		50
+#define	PATH_MAX		200
 
 // command line parameters...
 //  -B put dummy command in clipboard
@@ -51,7 +51,10 @@ void parseCommand( void );
 char * lexString = NULL;
 
 string	str;
-char *	args[ 20 ];		// 20 max args
+char *	thePgm = NULL;
+char	thePath[ PATH_MAX ];
+
+char *	args[ 30 ];		// 30 max args
 
 
 // foutput to stdout unless a filename is given;
@@ -60,7 +63,7 @@ FILE *	fout = NULL;
 char	fname[ PATH_MAX ];
 
 char	fn[ PATH_MAX ];
-char *	name;
+char *	name = NULL;
 char *	command = NULL;
 key_t	key;
 int		semflg;
@@ -134,18 +137,42 @@ main( int argc, char * argv[] )
 	}
 
 if( verbose ) { cout << "after getopt " << "\n"; }
-if( name == NULL ) {
-cout << "no semaphore name after getopt " << "\n";
-}
+	if( name == NULL ) {
+		cerr << "No name specified for the semaphone" << endl ;
+		exit( -1 );
+	}
 
 	// always need the actual semaphore
-	sem_t *	semID = sem_open(  name, O_CREAT | O_RDWR,  S_IRWXU | S_IRWXU | S_IRWXO, 1 );
+	int	oflag = O_CREAT | O_RDWR;
+	mode_t	mode = S_IRWXU | S_IRWXG | S_IRWXO;
+	int	status;
+	sem_t *	semID;
+
+	struct stat sb;
+	char	devsem[ 20 ];
+	strcpy( devsem, "/dev/shm/sem." );
+	strcat( devsem, name );
+
+	cout << "Opening the semaphore: " << name << endl;
+	if ( stat( devsem, &sb) == -1) {
+		cout << "creating..." << endl;
+		semID = sem_open(  name, oflag,  mode, 1 );		// create the semaphore
+    } else {
+		cout << "opening..." << endl;
+		oflag = O_RDWR;
+		semID = sem_open(  name, oflag );				// do not create the semaphore
+	}
+
 	if( semID == SEM_FAILED ) {
-		cerr << "Sem open failed..." << "\n";
-		cerr << "error code: " << errno << "\t" << "\n" ;
+		cerr << "Sem open failed: " << name << ", oflag: " << hex  << oflag  << oct << ", mode: 0" << mode << endl ;
+		cerr << "error code: " << dec << errno << "\t" << "\n" ;
 		perror( "sema_open" );
 		exit( -1 );
 	}
+	cerr << "Sem open: " << name << ", oflag: " << hex  << oflag  << ", mode: 0" << oct << mode << dec << endl ;
+	int	semvalue;
+	int sv = sem_getvalue( semID, &semvalue);
+	cout << pid << ": sv is: " << sv << ", semvalue is " << semvalue << endl ;
 
 	if( command != NULL ) {		// execute the command
 		int	status = 0;
@@ -154,46 +181,66 @@ cout << "no semaphore name after getopt " << "\n";
 		lexString = command;
 		parseCommand();
 
-		sem_wait( semID );
-		cout << pid << ": got it!" << "\n";
+		int src = sem_wait( semID );		// wait for semaphore
+		cout << pid << ": src is: " << src << endl ;
+		sv = sem_getvalue( semID, &semvalue);
+		cout << pid << ": sv is: " << sv << ", semvalue is " << semvalue << endl ;
 
 		newP = fork();					// fork new process, parent waits for completion
 		if( newP < 0 ) {	// error
+			// SAME PROCESS..., but need to die off
 			sem_post( semID );		// release semaphore since we're quiting
+			cout << "Posting the semaphore: " << name << endl;
+			sem_close( semID );
+			cout << "Closing the semaphore: " << name << endl;
 			cerr << "Fork failed..." << "\n";
 			perror( "fork error: " );
 			exit( -1 );
 		} else if( newP == 0 ) {	// child
+			// NEW PROCESS...
 			// exec the command line...
-			cout << "The cmd: " << command << "\n";
-			exit(0);
-		} else {	// parent
+			//cout << "The cmd: " << command << "\n";
+			//cout << "The path: " << thePath << "\n";
+			//cout << "The pgm: " << thePgm << "\n";
+			//for( int jj=0; args[jj] != NULL; jj++ )
+				//cout << jj << " : " << args[jj] << endl;
+
+			execv( thePgm, args );
+			cerr << "execv failed: " << errno << endl;
+			perror( "execv failed... " );
+			exit( -1 );
+		} else {	// in the parent
+			// SAME PROCESS..., need to wait for kid to terminate
 			pid_t p = wait( & status );			// wait for the kid to finish, report ending status
 			if( p == newP ) {					// status is for the child
 				if( status == 0 ) {					// normal (expected ) return
 				} else {							// some status returned.
 					int	exitVal = WIFEXITED( status );	// basic exit status
 					if( exitVal != 0 ) {
-					endingStatus = WEXITSTATUS( status );		// exit status from child
+					rc = WEXITSTATUS( status );		// exit status from child
 					}
 				}
 			} else {							// not expected to happen
+				cerr << "This is not supposed to happen !!!" << endl;
 			}
 		}
 
-		sem_post( semID );		// parent waits
+		sem_post( semID );		// only the parent releases the semaphore
+		cout << "Posting the semaphore: " << name << endl;
 	}
 
 	if( nomore == true ) {		// remove semaphore
-		int rc = sem_unlink( name );
-		if( rc < 0 ) {
+		cout << "Removing the semaphore: " << name << endl;
+		int urc = sem_unlink( name );
+		if( urc < 0 ) {
 			cerr << "Could not unlink the " << name << " semaphore." << "\n";
 			cerr << "error code: " << errno << "\n";
 		}
 	}
 
 	sem_close( semID );
-	return rc;
+	cout << "Closing the semaphore: " << name << endl;
+	return rc;	// could be from us or the kid if we ran a command
 }
 
 //
@@ -207,7 +254,9 @@ usage() {
 	fprintf( stderr, "\n\n" );
 	fprintf( stderr, "\t%s - %s\n", "?", "Ask for help." );
 	fprintf( stderr, "\t%s - %s\n", "B","Put dummy command line in clipboard, for pasting....");
+	fprintf( stderr, "\t%s - %s\n", "c","command to run." );
 	fprintf( stderr, "\t%s - %s\n", "n","Job name." );
+	fprintf( stderr, "\t%s - %s\n", "u","unlink semaphone (name)." );
 	fprintf( stderr, "\t%s - %s\n", "v","Be verbose." );
 	fprintf( stderr, "\n\n" );
 }
@@ -247,12 +296,14 @@ int	arg = 0;
 
 	lexString = command;
 	while( val = yylex() ) {
-		cout << "val is " << val << " yytext is " << yytext << "\n";
-		if( arg == 0 ) {
-			cout << "Pgm is " << yytext << "\n";
+		//cout << "val is " << val << " yytext is " << yytext << "\n";
+		if( arg == 0 ) {			// first arg is the name of the program
+			//cout << "Pgm is " << yytext << "\n";
+			strncpy( thePath, yytext, PATH_MAX );
+			thePgm = thePath;
 		}
 		int len = strlen( yytext );
-		args[ arg ] = (char*) malloc( len );
+		args[ arg ] = (char*) malloc( len + 1 );
 		strncpy( args[ arg ], yytext, len );
 		arg++;
 	}
